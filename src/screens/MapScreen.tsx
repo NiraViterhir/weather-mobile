dd728dcc840878c5c62c2a0a949903828ea43a6d
nira.viterhir <nina.novoselska@gmail.com>
2025-10-30 16:37:50 +0200

    add user permissions and update api key

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -56,9 +56,9 @@ export default function MapScreen({navigation}: Props) {
 
   const initialRegion: Region = useMemo(
     () => ({
-      latitude: 37.78825,
-      longitude: -122.4324,
-      latitudeDelta: 0.0922,
+      latitude:  49.1728522589072,
+      longitude: 25.012891600077154,
+      latitudeDelta: 3,
       longitudeDelta: 0.0421,
     }),
     [],
